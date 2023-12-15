ece8406cb4128554d192cc02666f5e6f0e69e4a2
Vinicius Oliveira Santos <eovinicius10@gmail.com>
2023-12-15 02:10:46 -0300

    ✨ feat: add use case to remove products from stock

---
+++ src/application/use-cases/produt/remove-stock-product-use-case.ts
@@ -0,0 +1,31 @@
+import { Product } from '../../../domain/entities/product';
+import { ProductRepository } from '../../repositories/product-repository';
+import { UseCase } from '../use-case';
+
+interface input {
+  productId: string;
+  quantity: number;
+}
+
+interface output {
+  product: Product;
+}
+
+export class RemoveStockProductUseCase implements UseCase<input, output> {
+  constructor(private readonly productRepository: ProductRepository) {}
+  async execute(data: input): Promise<output> {
+    const product = await this.productRepository.findById(data.productId);
+
+    if (!product) {
+      throw new Error('Product not found');
+    }
+
+    product.removeStock(data.quantity);
+
+    this.productRepository.save(product);
+
+    return {
+      product
+    };
+  }
+}
