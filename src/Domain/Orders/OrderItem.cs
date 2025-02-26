8341569caaad0d59a331e4a975857d9490391246
Vinicius Oliveira <eovinicius10@gmail.com>
2025-02-26 20:36:48 -0300

    feat: adiciona propriedade preco unitario

--- src/Domain/Orders/OrderItem.cs
+++ src/Domain/Orders/OrderItem.cs
@@ -6,13 +6,14 @@ public sealed class OrderItem : Entity
 {
     public Guid ProductId { get; private set; }
     public decimal Price { get; private set; }
+    public decimal UnitPrice { get; private set; }
     public int Quantity { get; private set; }
 
-    public OrderItem(Guid productId, decimal price, int quantity)
+    public OrderItem(Guid productId, decimal unitPrice, int quantity)
     {
         ProductId = productId;
+        UnitPrice = unitPrice;
+        Price = unitPrice * quantity;
         Quantity = quantity;
-        Price = price;
     }
-
 }
