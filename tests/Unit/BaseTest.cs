706cd352425700568bf96a57b470e13fecb82500
Vinicius Oliveira <eovinicius10@gmail.com>
2025-02-26 21:25:39 -0300

    test: adiciona classe baseTest para facilitar testes de eventos de domínio

---
+++ tests/Unit/BaseTest.cs
@@ -0,0 +1,23 @@
+﻿using Bogus;
+
+using Domain.Abstractions;
+
+namespace Unit;
+
+public abstract class BaseTest
+{
+    protected static readonly Faker Faker = new();
+
+    public static T AssertDomainEventWasPublished<T>(AggregateRoot aggregate)
+        where T : IDomainEvent
+    {
+        T? domainEvent = aggregate.DomainEvents.OfType<T>().SingleOrDefault();
+
+        if (domainEvent is null)
+        {
+            throw new Exception($"{typeof(T).Name} was not published");
+        }
+
+        return domainEvent;
+    }
+}
