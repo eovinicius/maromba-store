aa4c7e3d4de199aa351d199ab1652f721ea77b52
Vinicius Oliveira <eovinicius10@gmail.com>
2025-02-26 22:30:13 -0300

    feat: adiciona método de criação para customer

--- src/Domain/Customers/Customer.cs
+++ src/Domain/Customers/Customer.cs
@@ -5,4 +5,14 @@ namespace Domain.Customers;
 public class Customer : AggregateRoot
 {
     public string Document { get; private set; }
+
+    public Customer(string document)
+    {
+        Document = document;
+    }
+
+    public static Customer Create(string document)
+    {
+        return new Customer(document);
+    }
 }
