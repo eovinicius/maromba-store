1bb2c8b815cefa132d26372d661477f0a97c1cce
Vinicius Oliveira <eovinicius10@gmail.com>
2025-02-26 22:30:50 -0300

    feat: adiciona método criacao para product

--- src/Domain/Products/Product.cs
+++ src/Domain/Products/Product.cs
@@ -6,4 +6,15 @@ public sealed class Product : AggregateRoot
 {
     public string Name { get; private set; }
     public decimal Price { get; private set; }
+
+    public Product(string name, decimal price)
+    {
+        Name = name;
+        Price = price;
+    }
+
+    public static Product Create(string name, decimal price)
+    {
+        return new Product(name, price);
+    }
 }
